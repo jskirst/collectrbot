5772e8028a10df8f4a12001ec4d1553ae86c1614
Jonathan Kirst <jc@blueigloo.us>
2013-07-08 11:36:40 -0400

    Removing encrypted id as auth for chrome plugin since you can't access pages that have been updated to real user id.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,28 @@
 class ApiController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :ensure_login_params_exist
+  
+  respond_to :json
+  
+  private
+  def ensure_login_params_exist
+    return unless params[:email].blank? or params[:password].blank?
+    render json: { success: :failure, message: "missing login parameters" }
+  end
+  
+  def authenticate
+    user = User.find_for_database_authentication(email: params[:email])
+    return invalid_login_attempt unless user
+ 
+    if user.valid_password?(params[:password])
+      @current_user = user
+    else
+      return false
+    end
+  end
+  
+  def invalid_login_attempt
+    warden.custom_failure!
+    render json: { success: :failure, message: "Error with your login or password" }
+  end
 end
--- app/controllers/api_pages_controller.rb
+++ app/controllers/api_pages_controller.rb
@@ -1,21 +1,21 @@
 class ApiPagesController < ApiController
-  respond_to :json
+  before_filter :authenticate
   
   # POST /api/pages
   def create
-    raise "Token missing." if params[:token].blank?
+    encrypted_id = current_user.encrypted_id(params[:password])
     params[:pages].each do |url, data|
       viewed = data["viewed"].to_i
       page = Page.find_by_url(url)
       page = Page.create(url: url, title: data["title"]) unless page
       
-      user_page = UserPage.where("page_id = ? and (user_id = ? or user_id = ?)", page.id, params[:token], current_user.id.to_s).first  
+      user_page = UserPage.where("page_id = ? and (user_id = ? or user_id = ?)", page.id, encrypted_id, current_user.id.to_s).first  
       if user_page
         if viewed > user_page.viewed and viewed < 300
           user_page.update_attribute(:viewed, viewed)
         end
       else
-        user_page = UserPage.create!(page_id: page.id, user_id: params[:token], viewed: viewed)
+        user_page = UserPage.create!(page_id: page.id, user_id: encrypted_id, viewed: viewed)
       end
     end
     render json: { status: :created }
